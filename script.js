bf87fa5af07edd6f276c38d81bde723eff0565de
danielbrierley <59896584+danielbrierley@users.noreply.github.com>
2022-01-27 13:40:20 +0000

    removed text margin

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var t;
+var timerInterval;
+let questions;
+let completed = [];
+
+//useful functions
+function randint(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function contains(item, array) {
+  c = false;
+  for (x = 0; x < array.length; x++) {
+    if (item == array[x]) {
+      c = true
+    }
+  }
+  return c;
+
+}
+
+//COOKIES
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -19,10 +43,75 @@ function getCookie(cname) {
     }
     return "";
 }
+
+
+function generateQuestionID() {
+  number = randint(0, 2);
+  if (completed.length == 3) {
+    alert('completed');
+    return -1;
+  }
+  else {
+    while (contains(number, completed)) {
+      number = randint(0, 2);
+    }
+    completed.push(number);
+    return number;
+  }
+}
+
+function answerClicked(ans) {
+  nextQuestion();
+}
+
+function nextQuestion() {
+  t = 10;
+  clearInterval(timerInterval);
+  index = generateQuestionID();
   
 
+  question = questions.questions[index];
+  questionElement = document.getElementById("question");
+  questionElement.innerHTML = question.question;
+
+  for (x = 0; x < question.answers.length; x++) {
+    answer = question.answers[x];
+    answerElement = document.getElementById("ans"+(x+1));
+    answerElement.innerHTML = answer;
+  }
+
+  timerInterval = setInterval(timer, 1000);
+  te = document.getElementById("timer");
+  te.innerHTML = t;
+}
+
+function timer() {
+    t -= 1;
+    te = document.getElementById("timer");
+    te.innerHTML = t;
+    if (t < 0) {
+      nextQuestion();
+    }
+}
+
 function start() {
+    const getJSON = async url => {
+      const response = await fetch(url);
+      if(!response.ok) // check if response worked (no 404 errors etc...)
+        throw new Error(response.statusText);
+    
+      const data = response.json(); // get JSON from the response
+      return data; // returns a promise, which resolves to this data value
+    }
+    
+    console.log("Fetching data...");
+    getJSON("test.json").then(data => {
+      questions = data;
+      nextQuestion();
+    }).catch(error => {
+      console.error(error);
+    });
+
     var cookie = document.getElementById("cookie");
     cookie.innerHTML = getCookie('test');
-
 }
