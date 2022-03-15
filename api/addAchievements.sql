42d90623cef5e30eac6938543a221d85fccfcdd8
danielbrierley <59896584+danielbrierley@users.noreply.github.com>
2022-03-15 14:18:41 +0000

    fixes

--- api/addAchievements.sql
+++ api/addAchievements.sql
@@ -1,4 +1,8 @@
 INSERT INTO achievements (name, description)
 VALUES
-('Achievement3', 'This is a test 5'),
-('Achievement4', 'This is a test 6')
+("1957", "Start yout first quiz"),
+("Space Explorer", "Reach Neptune"),
+("Hot Streak", "Answer 5 questions in a row correctly"),
+("Rocket Malfunction", "Make 10 mistakes"),
+("Where Did This Come From?", "Collect 25 meteorites");
+
--- main.py
+++ main.py
@@ -285,5 +285,5 @@ def userList():
 #    return 'test'
 
 if __name__ == '__main__':
-    #app.run()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run()
+    #app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
